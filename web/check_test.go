9e01d057161464a7c34cc3aceeb5a91f269beb8c
Jack Sullivan <jack@divergent.codes>
2023-04-10 17:27:50 -0700

    Check JWT and hash by headers instead of POST body

--- web/check_test.go
+++ web/check_test.go
@@ -1,8 +1,8 @@
 package web
 
 import (
-	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http/httptest"
 	"testing"
@@ -14,16 +14,14 @@ import (
 func Test_Check_ValidTokenAllowed_Success(t *testing.T) {
 	setupMockRedis()
 
-	httpMethod := "POST"
+	httpMethod := "GET"
 	urlPath := "/blocklist/check"
 	tokenString := generateTokenStringHS256(30)
-
-	// Request payload creation.
-	payloadJSON, _ := json.Marshal(map[string]string{"jwt": tokenString})
-	payloadBytes := bytes.NewBuffer(payloadJSON)
+	bearerTokenString := fmt.Sprintf("Bearer %s", tokenString)
 
 	// Build the request.
-	request := httptest.NewRequest(httpMethod, urlPath, payloadBytes)
+	request := httptest.NewRequest(httpMethod, urlPath, nil)
+	request.Header.Add("Authorization", bearerTokenString)
 	w := httptest.NewRecorder()
 
 	// Issue HTTP request to handler.
@@ -62,17 +60,14 @@ func Test_Check_ValidTokenAllowed_Success(t *testing.T) {
 func Test_Check_ValidHashAllowed_Success(t *testing.T) {
 	setupMockRedis()
 
-	httpMethod := "POST"
+	httpMethod := "GET"
 	urlPath := "/blocklist/check"
 	tokenString := generateTokenStringHS256(30)
 	hashString := crypto.Sha256FromString(tokenString)
 
-	// Request payload creation.
-	payloadJSON, _ := json.Marshal(map[string]string{"sha256": hashString})
-	payloadBytes := bytes.NewBuffer(payloadJSON)
-
 	// Build the request.
-	request := httptest.NewRequest(httpMethod, urlPath, payloadBytes)
+	request := httptest.NewRequest(httpMethod, urlPath, nil)
+	request.Header.Add("X-Jwtblock-Sha256", hashString)
 	w := httptest.NewRecorder()
 
 	// Issue HTTP request to handler.
